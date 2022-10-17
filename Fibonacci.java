c0bb0228393c25ab2a32193ab1d1b57bc99e2941
Mayank Daiya <mayankdaiya03@gmail.com>
2022-10-17 19:03:27 +0530

    first commit

---
+++ Fibonacci.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Fibonacci{
+    public static int getFibonacci(int n){
+        if(n<2) return n;
+        return getFibonacci(n-1)+getFibonacci(n-2);
+    }
+
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(getFibonacci(n));
+    }
+}
