a7ab329c7ec3fc16e6abf2c17dd9bdc5628c9c26
Mayank Daiya <mayankdaiya03@gmail.com>
2022-10-17 20:12:10 +0530

    Recursion

--- SearchBinary.java
+++ SearchBinary.java
@@ -2,6 +2,7 @@ import java.util.*;
 //only for sorted array
 class SearchBinary{
     public static int search(int arr[],int si,int ei,int n){
+        if(si>ei) return -1;
         int mid=si+(ei-si)/2;
         if(arr[mid]>n) ei=mid-1;
         else if(n>arr[mid]) si=mid+1;
@@ -15,6 +16,6 @@ class SearchBinary{
         int arr[]=new int[len];
         for(int i=0;i<len;i++) arr[i]=sc.nextInt();
         int n=sc.nextInt();
-        System.out.println(search(arr,0,len,n));
+        System.out.println(search(arr,0,len-1,n));
     }
 }
