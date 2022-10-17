a9376c841790b12fa9c3b4259306fb7ee25fb712
Mayank Daiya <mayankdaiya03@gmail.com>
2022-10-17 19:47:14 +0530

    Recursion

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-class Fibonacci{
+class Solution{
     public static int getFibonacci(int n){
         if(n<2) return n;
         return getFibonacci(n-1)+getFibonacci(n-2);
---
+++ SearchBinary.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class SearchBinary{
+    public static int search(int arr[],int si,int ei,int n){
+        int mid=si+(ei-si)/2;
+        if(arr[mid]>n) ei=mid-1;
+        else if(n>arr[mid]) si=mid+1;
+        else return mid;
+        return search(arr,si,ei,n);
+    }
+
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int len=sc.nextInt();
+        int arr[]=new int[len];
+        for(int i=0;i<len;i++) arr[i]=sc.nextInt();
+        int n=sc.nextInt();
+        System.out.println(search(arr,0,len,n));
+    }
+}
