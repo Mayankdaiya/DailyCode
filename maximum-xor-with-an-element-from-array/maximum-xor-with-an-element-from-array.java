dde0822300e872ac17d3d83fa5ebca252176fd2e
Mayank Daiya <74089887+Mayankdaiya@users.noreply.github.com>
2023-07-06 11:39:16 +0530

    Time: 129 ms (100.00%) | Memory: 107.9 MB (92.56%) - LeetSync

---
+++ maximum-xor-with-an-element-from-array/maximum-xor-with-an-element-from-array.java
@@ -0,0 +1,88 @@
+class Solution {
+    public static class Pair{
+        int xi;
+        int mi;
+        int qi;
+        Pair(int xi,int mi,int qi){
+            this.xi=xi;
+            this.mi=mi;
+            this.qi=qi;
+        }
+    }
+    public static class Node{
+        Node zero;
+        Node one;
+    }
+    public static void insert(Node root, int val){
+        Node curr=root;
+        for(int i=31;i>=0;i--){
+            if((val&(1<<i))==0){
+                if(curr.zero!=null){
+                    curr=curr.zero;
+                } else{
+                    Node nn=new Node();
+                    curr.zero=nn;
+                    curr=nn;
+                }
+            } else {
+                if(curr.one!=null){
+                    curr=curr.one;
+                } else{
+                    Node nn=new Node();
+                    curr.one=nn;
+                    curr=nn;
+                }
+            }
+        }
+    }
+    public static int getMaxXor(Node root, int val){
+        int num=0;
+        Node curr=root;
+        for(int i=31;i>=0;i--){
+            int bit=(val&(1<<i));
+            if(bit==0){
+                if(curr.one!=null){
+                    num+=(1<<i);
+                    curr=curr.one;
+                } else{
+                    curr=curr.zero;
+                }
+            } else{
+                if(curr.zero!=null){
+                    num+=(1<<i);
+                    curr=curr.zero;
+                } else{
+                    curr=curr.one;
+                }
+            }
+        }
+        return num;
+    }
+    public int[] maximizeXor(int[] arr, int[][] qr) {
+        Arrays.sort(arr);
+        Pair[] p=new Pair[qr.length];
+        for(int i=0;i<p.length;i++){
+            p[i]=new Pair(qr[i][0],qr[i][1],i);
+        }
+        Arrays.sort(p,new Comparator<Pair>(){
+            public int compare(Pair o1,Pair o2){
+                return o1.mi-o2.mi;
+            }
+        });
+        Node root=new Node();
+        int ans[]=new int[p.length];
+        int j=0;
+        for(int i=0;i<ans.length;i++){
+            while(j<arr.length && arr[j]<=p[i].mi){
+                insert(root,arr[j]);
+                j++;
+            }
+            if(j==0){
+                ans[p[i].qi]=-1;
+            } else{
+                ans[p[i].qi]=getMaxXor(root,p[i].xi);
+            }
+        }
+        return ans;
+    }
+}
