a31a9084fd769721a2cb63e2ed2cecfd87da3056
Mayank Daiya <74089887+Mayankdaiya@users.noreply.github.com>
2023-07-16 19:25:04 +0530

    Time: 8 ms (53.95%) | Memory: 85.7 MB (70.18%) - LeetSync

--- 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.java
+++ 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.java
@@ -1,10 +1,9 @@
 class Solution {
     public int[] vowelStrings(String[] words, int[][] queries) {
         int freq[]=new int[words.length];
-        String c="aeiou";
         for(int i=0;i<words.length;i++){
             char ch1=words[i].charAt(0), ch2=words[i].charAt(words[i].length()-1);
-            if(c.indexOf(ch1)!=-1 && c.indexOf(ch2)!=-1){
+            if(check(ch1,ch2)){
                 freq[i]=1;
             }
             if(i!=0) freq[i]+=freq[i-1];
@@ -16,5 +15,9 @@ class Solution {
             else ans[i]=freq[en];
         }
         return ans;
+    }
+    public static boolean check(char ch1,char ch2){
+        if((ch1=='a' || ch1=='e' || ch1=='i' || ch1=='o' || ch1=='u') && (ch2=='a' || ch2=='e' || ch2=='i' || ch2=='o' || ch2=='u')) return true;
+        return false;
     }
 }
