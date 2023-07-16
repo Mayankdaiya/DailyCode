e76de7691605cbb0ee80e947d6370cff6e57df68
Mayank Daiya <74089887+Mayankdaiya@users.noreply.github.com>
2023-07-16 19:21:59 +0530

    Time: 9 ms (50.00%) | Memory: 86.1 MB (40.79%) - LeetSync

--- 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.java
+++ 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.java
@@ -9,14 +9,11 @@ class Solution {
             }
             if(i!=0) freq[i]+=freq[i-1];
         }
-        System.out.println(Arrays.toString(freq));
         int ans[]=new int[queries.length];
         for(int i=0;i<queries.length;i++){
             int st=queries[i][0], en=queries[i][1];
             if(st!=0) ans[i]=freq[en]-freq[st-1];
             else ans[i]=freq[en];
-            
-            // if(st==en) ans[i]=freq[st];
         }
         return ans;
     }
