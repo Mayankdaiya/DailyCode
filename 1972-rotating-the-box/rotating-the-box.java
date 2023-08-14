80950e376971e10cd97a8e5bc93efc0c14830019
Mayank Daiya <74089887+Mayankdaiya@users.noreply.github.com>
2023-08-14 18:37:53 +0530

    Time: 5 ms (100.00%) | Memory: 68.9 MB (75.27%) - LeetSync

--- 1972-rotating-the-box/rotating-the-box.java
+++ 1972-rotating-the-box/rotating-the-box.java
@@ -1,33 +1,15 @@
 class Solution {
     public char[][] rotateTheBox(char[][] box) {
-        for(int i=0;i<box.length;i++){
-            int in=-1;
-            for(int j=0;j<box[i].length;j++){
-                if(box[i][j]=='#'){
-                    if(in==-1 || (in<box[0].length && box[i][in]=='#')){
-                        in=j;
-                        while(in<box[0].length-1){
-                            if(box[i][in]=='.' || box[i][in]=='*') break;
-                            in++;
-                        }
-                    }
-                    if(in==box[0].length-2) System.out.println("fds"+j);
-                    if(in<box[0].length && box[i][in]=='.'){
-                        box[i][j]='.'; box[i][in]='#';
-                        in++;
-                    } 
-                } else if(box[i][j]=='*') in=-1;
+    int m = box.length, n = box[0].length;
+    char [][] res = new char[n][m];
+    for (int i = 0; i < m; ++i)
+        for (int j = n - 1, k = n - 1; j >= 0; --j) {
+            res[j][m - i - 1] = '.';
+            if (box[i][j] != '.') {
+                k = box[i][j] == '*' ? j : k;
+                res[k--][m - i - 1] = box[i][j];
             }
         }
-        for(int i=0;i<box.length;i++){
-            System.out.println(Arrays.toString(box[i]));
-        }
-        char ans[][]=new char[box[0].length][box.length];
-        for(int i=0;i<box[0].length;i++){
-            for(int j=0;j<box.length;j++){
-                ans[i][j]=box[box.length-1-j][i];
-            }
-        }
-        return ans;
-    }
+    return res;
+}
 }
