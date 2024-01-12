3543c231620e2129fc522cddaa5654ea7d114519
Mayank Daiya <74089887+Mayankdaiya@users.noreply.github.com>
2024-01-12 09:40:07 +0530

    Time: 5 ms (51.60%) | Memory: 41.9 MB (19.43%) - LeetSync

--- 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.java
+++ 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.java
@@ -1,11 +1,12 @@
 class Solution {
     public boolean halvesAreAlike(String s) {
-        int f=1, c=0;
-        for(int i=0;i<s.length();i++){
-            if(i==s.length()/2) f=-1;
-            char ch=Character.toLowerCase(s.charAt(i));
-            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u') c+=f;
+        int c1=0, c2=0;
+        for(int i=0;i<s.length()/2;i++){
+            char ch1=Character.toLowerCase(s.charAt(i));
+            char ch2=Character.toLowerCase(s.charAt(s.length()-i-1));
+            if(ch1=='a' || ch1=='e' || ch1=='i' || ch1=='o' || ch1=='u') c1++;
+            if(ch2=='a' || ch2=='e' || ch2=='i' || ch2=='o' || ch2=='u') c2++;
         }
-        return c==0;
+        return c1==c2;
     }
 }
