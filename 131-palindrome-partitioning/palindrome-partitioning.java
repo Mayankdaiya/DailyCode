4c3dc31a618c5f5d8982ccab416c04526cf0682d
Mayank Daiya <74089887+Mayankdaiya@users.noreply.github.com>
2024-05-22 18:52:16 +0530

    Time: 7 ms (96.51%) | Memory: 56.7 MB (65.31%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,14 +1,17 @@
 class Solution {
     public List<List<String>> partition(String s) {
         List<List<String>> result = new ArrayList<>();
-        List<String> curr = new ArrayList<>();
+        List<String> cur=new ArrayList<>();
         int lenS = s.length();
-
-        explore(0, s, curr, result, lenS);
+        int dp[][]=new int[lenS][lenS];
+        for(int i=0;i<lenS;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        explore(0, s, cur, result, lenS, dp);
         return result;
     }
 
-    private void explore(int index, String s, List<String> curr, List<List<String>> result, int lenS) {
+    private void explore(int index, String s, List<String> curr, List<List<String>> result, int lenS, int [][]dp) {
         if (index >= lenS) {
             result.add(new ArrayList<>(curr));
             return;
@@ -16,22 +19,22 @@ class Solution {
 
         for (int i = index; i < lenS; ++i) {
             String subStr = s.substring(index, i + 1);
-            if (isPalindrome(subStr)) {
+            if (isPalindrome(s,index,i,dp)) {
                 curr.add(subStr);
-                explore(i + 1, s, curr, result, lenS);
+                explore(i + 1, s, curr, result, lenS, dp);
                 curr.remove(curr.size() - 1);
             }
         }
     }
 
-    private boolean isPalindrome(String s) {
-        int left = 0, right = s.length() - 1;
-        while (left < right) {
-            if (s.charAt(left) != s.charAt(right))
-                return false;
-            left++;
-            right--;
+    private boolean isPalindrome(String s,int i, int j,int dp[][]) {
+        if(dp[i][j]!=-1) return dp[i][j]==1;
+        if(i>=j) dp[i][j]=1;
+        else if(s.charAt(i)==s.charAt(j)){
+            dp[i][j]=isPalindrome(s,i+1,j-1,dp)?1:0;
+        } else{
+            dp[i][j]=0;
         }
-        return true;
+        return dp[i][j]==1;
     }
 }
