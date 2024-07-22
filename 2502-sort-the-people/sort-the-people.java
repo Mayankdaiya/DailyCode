c027f269886550314a2cb2143f7b643d5b1ac125
Mayank Daiya <74089887+Mayankdaiya@users.noreply.github.com>
2024-07-22 07:51:57 +0530

    Time: 10 ms (48.32%) | Memory: 45.4 MB (18.45%) - LeetSync

--- 2502-sort-the-people/sort-the-people.java
+++ 2502-sort-the-people/sort-the-people.java
@@ -1,26 +1,13 @@
 class Solution {
-    class Pair{
-        String name;
-        int height;
-        Pair(String n,int h){
-            this.name=n;
-            this.height=h;
-        }
-    }
     public String[] sortPeople(String[] names, int[] heights) {
-        Pair res[]=new Pair[names.length];
+        Map<Integer,String> map=new TreeMap<>(Collections.reverseOrder());
         for(int i=0;i<names.length;i++){
-            res[i]=new Pair(names[i],heights[i]);
+            map.put(heights[i],names[i]);
         }
-        Arrays.sort(res,new Comparator<Pair>(){
-            public int compare(Pair t1,Pair t2){
-                return t2.height-t1.height;
-            }
-        });
-        String ans[]=new String[res.length];
-        for(int i=0;i<res.length;i++){
-            ans[i]=res[i].name;
+        int i=0;
+        for(int key:map.keySet()){
+            names[i++]=map.get(key);
         }
-        return ans;
+        return names;
     }
 }
