1749cf981b57f7e56d360e60e9715f93b93e96c4
Mayank Daiya <74089887+Mayankdaiya@users.noreply.github.com>
2024-10-11 09:06:44 +0530

    Time: 34 ms (97.66%) | Memory: 50.5 MB (41.52%) - LeetSync

--- 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.java
+++ 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.java
@@ -1,35 +1,19 @@
 class Solution {
-    public int smallestChair(int[][] times, int targetFriend) {
-        int n = times.length;
-        List<int[]> events = new ArrayList<>();
+    public int smallestChair(int[][] times, int target) {
+        int n = times.length, max = -1, end = -1, start = times[target][0];
+        Arrays.sort(times, (a, b) -> a[0] - b[0]);
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        PriorityQueue<Integer> seats = new PriorityQueue<>();
         for (int i = 0; i < n; i++) {
-            events.add(new int[]{times[i][0], i, 1});  
-            events.add(new int[]{times[i][1], i, 0});  
-        }
-        Collections.sort(events, (a, b) -> a[0] == b[0] ? Integer.compare(a[2], b[2]) : Integer.compare(a[0], b[0]));
-
-        PriorityQueue<Integer> availableChairs = new PriorityQueue<>();
-        for (int i = 0; i < n; i++) {
-            availableChairs.offer(i); 
-        }
-
-        int[] friendToChair = new int[n];
-
-        for (int[] event : events) {
-            int time = event[0];
-            int friend = event[1];
-            int action = event[2];  
-            
-            if (action == 1) { 
-                int chair = availableChairs.poll();
-                friendToChair[friend] = chair;
-                if (friend == targetFriend) {
-                    return chair;  
-                }
-            } else { 
-                availableChairs.offer(friendToChair[friend]);
+            while (!pq.isEmpty() && pq.peek()[0] <= times[i][0]) {
+                seats.offer(pq.poll()[1]);
+            }
+            end = seats.isEmpty() ? ++max : seats.poll();            
+            pq.offer(new int[]{times[i][1], end});
+            if (times[i][0] == start) {
+                break;
             }
         }
-        return -1;
+        return end;
     }
 }
