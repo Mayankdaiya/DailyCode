624fa5f634c50935b5f2aa011746364afe65a644
Mayank Daiya <74089887+Mayankdaiya@users.noreply.github.com>
2024-11-18 08:29:16 +0530

    Time: 1 ms (71.00%) | Memory: 42.1 MB (72.20%) - LeetSync

---
+++ 1755-defuse-the-bomb/defuse-the-bomb.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] decrypt(int[] code, int k) {
+        int n=code.length;
+        if(k==0) return new int[n];
+        int res[]=new int[n];
+        int inc=k>0?1:-1, j=(n+inc)%n, curr=0;
+        for(int i=0;i<Math.abs(k);i++){
+            curr+=code[j];
+            j=(j+inc)%n;
+        }
+        System.out.println(curr);
+        for(int i=0;i<n;i++){
+            res[i]=curr;
+            if(k>0){
+                curr+=code[j];
+                curr-=code[(i+1)%n];
+                j=(j+1)%n;
+            } else {
+                curr+=code[i];
+                curr-=code[++j%n];
+            }
+        }
+        return res;
+    }
+}
