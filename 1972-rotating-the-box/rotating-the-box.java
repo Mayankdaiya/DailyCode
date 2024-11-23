d18290b94af2e69676de854d979f4e551b3841dd
Mayank Daiya <74089887+Mayankdaiya@users.noreply.github.com>
2024-11-23 08:39:14 +0530

    Time: 19 ms (26.83%) | Memory: 79.1 MB (85.79%) - LeetSync

--- 1972-rotating-the-box/rotating-the-box.java
+++ 1972-rotating-the-box/rotating-the-box.java
@@ -1,15 +1,30 @@
 class Solution {
     public char[][] rotateTheBox(char[][] box) {
-    int m = box.length, n = box[0].length;
-    char [][] res = new char[n][m];
-    for (int i = 0; i < m; ++i)
-        for (int j = n - 1, k = n - 1; j >= 0; --j) {
-            res[j][m - i - 1] = '.';
-            if (box[i][j] != '.') {
-                k = box[i][j] == '*' ? j : k;
-                res[k--][m - i - 1] = box[i][j];
+        int m=box.length, n=box[0].length;
+        char arr[][]=new char[n][m];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n-1;j++){
+                int t=j+1;
+                if(box[i][j]=='#'){
+                    if(box[i][t]=='.'){
+                        box[i][t]='#';
+                        box[i][j]='.';
+                    } else if(box[i][t]=='#'){
+                        while(t<n && box[i][t]=='#'){
+                            t++;
+                        }
+                        if(t<n && box[i][t]=='.'){
+                            box[i][t]='#';
+                            box[i][j]='.';
+                        } 
+                    }
+                }
+            }
+            int k=m-i-1;
+            for(int j=0;j<n;j++){
+                arr[j][k]=box[i][j];
             }
         }
-    return res;
-}
+        return arr;
+    }
 }
