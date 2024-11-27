6815a0da0e0205d55107ebe77aafa583c157cdd1
Mayank Daiya <74089887+Mayankdaiya@users.noreply.github.com>
2024-11-27 19:06:02 +0530

    Time: 97 ms (86.47%) | Memory: 45.8 MB (15.95%) - LeetSync

--- 3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.java
+++ 3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.java
@@ -1,44 +1,46 @@
 class Solution {
-    private void updateDistances(List<List<Integer>> graph, int current, int[] distances) {
-        int newDist = distances[current] + 1;
-        
-        for (int neighbor : graph.get(current)) {
-            if (distances[neighbor] <= newDist) continue;
-            
-            distances[neighbor] = newDist;
-            updateDistances(graph, neighbor, distances);
+    private int BFS(int n,Map<Integer,List<Integer>> adV){
+        Queue<Integer> q=new LinkedList<>();
+        q.add(0);
+        boolean[] visited=new boolean[n];
+        visited[0]=true;
+        int d=0;
+        while(!q.isEmpty()){
+            int l=q.size();
+            for(int i=0;i<l;i++){
+                int node=q.poll();
+                for(int j:adV.get(node)){
+                    if(!visited[j]){
+                        q.add(j);
+                        visited[j]=true;
+                    }
+                }
+            }
+            d++;
+            if(q.contains(n-1)){
+                return d;
+            }
         }
+        return -1;
     }
-    
     public int[] shortestDistanceAfterQueries(int n, int[][] queries) {
-        int[] distances = new int[n];
-        for (int i = 0; i < n; ++i) {
-            distances[i] = n - 1 - i;
+        Map<Integer,List<Integer>> adV=new HashMap<>();
+        for(int i=0;i<n;i++){
+            adV.put(i,new ArrayList<>());
+            if(i<n-1){
+                adV.get(i).add(i+1);
+            }
         }
-        
-        List<List<Integer>> graph = new ArrayList<>();
-        for (int i = 0; i < n; ++i) {
-            graph.add(new ArrayList<>());
+        List<Integer> result=new ArrayList<>();
+        for(int[] query:queries){
+            int u=query[0],v=query[1];
+            adV.get(u).add(v);
+            result.add(BFS(n,adV));
         }
-        
-        for (int i = 0; i + 1 < n; ++i) {
-            graph.get(i + 1).add(i);
+        int[] intArray=new int[result.size()];
+        for (int i=0;i<result.size();i++) {
+            intArray[i]=result.get(i);
         }
-        
-        int[] answer = new int[queries.length];
-        int queryIdx = 0;
-        
-        for (int[] query : queries) {
-            int source = query[0];
-            int target = query[1];
-            
-            graph.get(target).add(source);
-            distances[source] = Math.min(distances[source], distances[target] + 1);
-            updateDistances(graph, source, distances);
-            
-            answer[queryIdx++] = distances[0];
-        }
-        
-        return answer;
+        return intArray;
     }
 }
