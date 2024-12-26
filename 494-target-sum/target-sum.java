cb18d65559828337bb8d2f0d2061134c45b27bf7
Mayank Daiya <74089887+Mayankdaiya@users.noreply.github.com>
2024-12-26 08:57:42 +0530

    Time: 9 ms (64.77%) | Memory: 44.8 MB (27.87%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,10 +1,19 @@
 class Solution {
+    static int totalsum;
     public int findTargetSumWays(int[] nums, int target) {
-        return solve(nums,0,0,target);
+        totalsum=Arrays.stream(nums).sum();
+        int dp[][]=new int[nums.length][totalsum*2+1];
+        for(int row[]:dp){
+            Arrays.fill(row,Integer.MIN_VALUE);
+        }
+        return solve(nums,0,0,target,dp);
     }
-    public static int solve(int nums[],int in,int sum,int target){
-        if(in==nums.length && sum==target) return 1;
-        else if((in==nums.length && sum!=target) || in==nums.length) return 0;
-        return solve(nums,in+1,sum+nums[in],target)+solve(nums,in+1,sum-nums[in],target);
+    public static int solve(int nums[],int in,int sum,int target,int dp[][]){
+        if(in==nums.length){
+            if(sum==target) return 1;
+            else return 0;
+        } 
+        if(dp[in][sum+totalsum]!=Integer.MIN_VALUE) return dp[in][sum+totalsum];
+        return dp[in][sum+totalsum]=solve(nums,in+1,sum+nums[in],target,dp)+solve(nums,in+1,sum-nums[in],target,dp);
     }
 }
