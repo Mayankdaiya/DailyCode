c47d61f77f838923f7176dc1621b59977ef2b867
Mayank Daiya <74089887+Mayankdaiya@users.noreply.github.com>
2024-12-27 18:43:31 +0530

    Time: 9 ms (37.34%) | Memory: 50.4 MB (13.95%) - LeetSync

--- 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.java
+++ 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.java
@@ -1,37 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-
     public TreeNode reverseOddLevels(TreeNode root) {
-        if (root == null) {
-            return null; 
-        }
-
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        int level = 0;
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            List<TreeNode> currentLevelNodes = new ArrayList<>();
-            for (int i = 0; i < size; i++) {
-                TreeNode node = queue.poll();
-                currentLevelNodes.add(node);
-
-                if (node.left != null) queue.add(node.left);
-                if (node.right != null) queue.add(node.right);
-            }
-            if (level % 2 == 1) {
-                int left = 0, right = currentLevelNodes.size() - 1;
-                while (left < right) {
-                    int temp = currentLevelNodes.get(left).val;
-                    currentLevelNodes.get(left).val = currentLevelNodes.get(
-                        right
-                    ).val;
-                    currentLevelNodes.get(right).val = temp;
-                    left++;
-                    right--;
+        TreeNode res=root;
+        Queue<TreeNode> q=new LinkedList<>();
+        q.add(root);
+        int f=0;
+        while(!q.isEmpty()){
+            int s=q.size();
+            if(f==0){
+                List<TreeNode> list=new ArrayList<>();
+                while(s-->0){
+                    TreeNode t=q.poll();
+                    if(t.left==null) continue;
+                    list.add(t.left); 
+                    list.add(t.right);
+                    q.add(t.left);
+                    q.add(t.right);
                 }
+                int n=list.size();
+                for(int i=0;i<n/2;i++){
+                    int t=list.get(i).val;
+                    list.get(i).val=list.get(n-i-1).val;
+                    list.get(n-i-1).val=t;
+                }
+                f=1;
+            } else {
+                while(s-->0){
+                    TreeNode t=q.poll();
+                    if(t.left==null) continue;
+                    q.add(t.left);
+                    q.add(t.right);
+                }
+                f=0;
             }
-
-            level++;
         }
         return root;
     }
