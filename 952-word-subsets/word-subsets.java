6dac8c91e5327765086ea159242368779427d456
Mayank Daiya <74089887+Mayankdaiya@users.noreply.github.com>
2025-01-10 09:20:54 +0530

    Time: 14 ms (50.00%) | Memory: 53.6 MB (94.19%) - LeetSync

--- 952-word-subsets/word-subsets.java
+++ 952-word-subsets/word-subsets.java
@@ -1,27 +1,26 @@
 class Solution {
     public List<String> wordSubsets(String[] words1, String[] words2) {
-        Map<Character,Integer> freq=new HashMap<>();
+        int freq[]=new int[26];
         for(int i=0;i<words2.length;i++){
             String s=words2[i];
-            Map<Character,Integer> map=new HashMap<>();
+            int map[]=new int[26];
             for(int j=0;j<s.length();j++){
-                map.put(s.charAt(j),map.getOrDefault(s.charAt(j),0)+1);
+                map[s.charAt(j)-'a']++;
             }
-            for(char k:map.keySet()){
-                if(!freq.containsKey(k)) freq.put(k,map.get(k));
-                else freq.put(k,Math.max(map.get(k),freq.get(k)));
+            for(int j=0;j<26;j++){
+                freq[j]=Math.max(freq[j],map[j]);
             }
         }
         List<String> list=new ArrayList<>();
         for(int i=0;i<words1.length;i++){
             String s=words1[i];
-            Map<Character,Integer> map=new HashMap<>();
+            int map[]=new int[26];
             for(int j=0;j<s.length();j++){
-                map.put(s.charAt(j),map.getOrDefault(s.charAt(j),0)+1);
+                map[s.charAt(j)-'a']++;
             }
             int f=0;
-            for(char k:freq.keySet()){
-                if(!map.containsKey(k) || map.get(k)<freq.get(k)){
+            for(int j=0;j<26;j++){
+                if(freq[j]!=0 && map[j]<freq[j]){
                     f=1;
                     break;
                 }
