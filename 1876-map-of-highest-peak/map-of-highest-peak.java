3c1ce181ad0dafc5a9fefcd8c5ea50862db069ba
Mayank Daiya <74089887+Mayankdaiya@users.noreply.github.com>
2025-01-22 08:29:47 +0530

    Time: 13 ms (99.81%) | Memory: 151 MB (91.43%) - LeetSync

--- 1876-map-of-highest-peak/map-of-highest-peak.java
+++ 1876-map-of-highest-peak/map-of-highest-peak.java
@@ -1,34 +1,36 @@
 class Solution {
-    public int[][] highestPeak(int[][] isWater) {
-        int n=isWater.length, m=isWater[0].length;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(isWater[i][j]==1) isWater[i][j]=0;
-                else isWater[i][j]=-1;
-            }
+    public int[][] highestPeak(int[][] wat) {
+        if (wat.length == 0 || wat[0].length == 0) {
+            return wat;
         }
-        int t=0;
-        while(true){
-            if(change(isWater,t)){
-                t++;
-            } else break;
-        }
-        return isWater;
-    }
-    public static boolean change(int mat[][],int t){
-        int n=mat.length, m=mat[0].length, f=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(mat[i][j]==t){
-                    if(i+1<n && mat[i+1][j]==-1) mat[i+1][j]=t+1;
-                    if(j+1<m && mat[i][j+1]==-1) mat[i][j+1]=t+1;
-                    if(i-1>=0 && mat[i-1][j]==-1) mat[i-1][j]=t+1;
-                    if(j-1>=0 && mat[i][j-1]==-1) mat[i][j-1]=t+1;
-                    f=1;
+
+        int m = wat.length, n = wat[0].length;
+        var res = new int[m][n];
+        var maxRange = m * n;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (wat[i][j] == 1) {
+                    res[i][j] = 0;
+                } else {
+                    var left = j > 0 ? res[i][j - 1] : maxRange;
+                    var up = i > 0 ? res[i - 1][j] : maxRange;
+                    res[i][j] = Math.min(left, up) + 1;
                 }
             }
         }
-        return f==1;
-    }
 
+        for (int i = m - 1; i >= 0; --i) {
+            for (int j = n - 1; j >= 0; --j) {
+                if (wat[i][j] == 1) {
+                    res[i][j] = 0;
+                } else {
+                    var right = (j < n - 1) ? res[i][j + 1] : maxRange;
+                    var down = (i < m - 1) ? res[i + 1][j] : maxRange;
+                    res[i][j] = Math.min(Math.min(right, down) + 1, res[i][j]);
+                }
+            }
+        }
+
+        return res;
+    }
 }
