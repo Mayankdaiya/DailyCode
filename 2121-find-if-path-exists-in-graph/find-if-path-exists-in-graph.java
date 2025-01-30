ea7f7369607ac6b0ecaa57fb45e889208ba3672a
Mayank Daiya <74089887+Mayankdaiya@users.noreply.github.com>
2025-01-30 10:31:47 +0530

    Time: 143 ms (52.26%) | Memory: 162.3 MB (55.79%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,20 +1,24 @@
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        if(edges.length == 0) return true;
-        boolean[] visited = new boolean[n];
-        boolean flag = true;
-        visited[source] = true;
-    while(flag){
-        flag = false;
-        for(int[] edge : edges){
-            if(visited[edge[0]] != visited[edge[1]]){
-                visited[edge[0]] = true;
-                visited[edge[1]] = true;
-                flag = true;
-            }
-            if(visited[destination]) return true;
+        Map<Integer,List<Integer>> map=new HashMap<>();
+        for(int i=0;i<edges.length;i++){
+            if(!map.containsKey(edges[i][0])) map.put(edges[i][0],new ArrayList<>());
+            if(!map.containsKey(edges[i][1])) map.put(edges[i][1],new ArrayList<>());
+            map.get(edges[i][0]).add(edges[i][1]);
+            map.get(edges[i][1]).add(edges[i][0]);
         }
-    }
-    return false;
+        Queue<Integer> q=new LinkedList<>();
+        boolean vis[]=new boolean[n];
+        q.add(source);
+        while(!q.isEmpty()){
+            int ed=q.poll();
+            if(ed==destination) return true;
+            if(vis[ed] || !map.containsKey(ed)) continue;
+            vis[ed]=true;
+            for(int t:map.get(ed)){
+                q.add(t);
+            }
+        }
+        return false;
     }
 }
