d04e00a2517ca8b07e567c5cae850e443c472016
Mayank Daiya <74089887+Mayankdaiya@users.noreply.github.com>
2025-03-18 09:20:07 +0530

    Time: 3 ms (98.64%) | Memory: 55.8 MB (57.77%) - LeetSync

--- 2478-longest-nice-subarray/longest-nice-subarray.java
+++ 2478-longest-nice-subarray/longest-nice-subarray.java
@@ -1,31 +1,15 @@
 class Solution {
     public int longestNiceSubarray(int[] nums) {
-        int in=0, res=1;
-        int bit[]=new int[33];
-        for(int i=0;i<nums.length;i++){
-            change(bit,nums[i],1);
-            while(in<i && !isbitone(bit)){
-                change(bit,nums[in],-1);
-                in++;
+        int start=0, cur=nums[0], res=1;
+        for(int i=1;i<nums.length;i++){
+            int n=nums[i];
+            while((cur&n)!=0){
+                cur=cur^nums[start];
+                start++;
             }
-            res=Math.max(res,i-in+1);
+            res=Math.max(i-start+1,res);
+            cur=cur|n;
         }
         return res;
-    }
-    public static void change(int bit[], int val, int op){
-        int p=1, in=1;
-        while(val>0){
-            if((val&p)!=0){
-                bit[in]+=op;
-                val-=p;
-            }
-            in++; p*=2;
-        }
-    }
-    public static boolean isbitone(int bit[]){
-        for(int i=1;i<33;i++){
-            if(bit[i]>1) return false;
-        }
-        return true;
     }
 }
