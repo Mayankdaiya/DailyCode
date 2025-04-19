bf71087a19fd7a66029930a68413bc97d3bbb015
Mayank Daiya <74089887+Mayankdaiya@users.noreply.github.com>
2025-04-19 10:21:53 +0530

    Time: 65 ms (34.08%) | Memory: 57.3 MB (71.77%) - LeetSync

--- 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.java
+++ 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.java
@@ -1,28 +1,26 @@
 class Solution {
     public long countFairPairs(int[] nums, int lower, int upper) {
         Arrays.sort(nums);
-        long res=0;
-        for(int i=0;i<nums.length-1;i++){
-            if(nums[i]+nums[nums.length-1]<lower || nums[i]+nums[i+1]>upper) continue;
-            int in=i+1, j=nums.length-1, l=-1, u=-1;
-            while(in<j){
-                int mid=in+(j-in)/2, sum=nums[i]+nums[mid];
-                if(sum<=upper) in=mid+1;
-                else if(sum>upper) j=mid-1;
-            }
-            if(u==-1) u=j;
-            while(u>=0 && nums[i]+nums[u]>upper) u--;
-            in=i+1; j=nums.length-1;
-            while(in<j){
-                int mid=in+(j-in)/2, sum=nums[i]+nums[mid];
-                if(sum>=lower) j=mid-1;
-                else if(sum<lower) in=mid+1;
-            }
-            if(l==-1) l=in;
-            while(l<nums.length && nums[i]+nums[l]<lower) l++;
-            // System.out.println(l+"-"+u);
-            res+=(u-l+1);
+        long pairs = 0;
+        for(int in=0;in<nums.length-1;in++){
+            //check for lower
+            int t1=cal(nums,in+1,nums.length-1,lower-nums[in]);
+            //check for upper
+            int t2=cal(nums,in+1,nums.length-1,upper-nums[in]+1);
+            pairs+=(t2-t1);
         }
-        return res;
+        return pairs;
+    }
+
+    public int cal(int nums[],int i,int j,int val){
+        while(i<=j){
+            int idx=i+(j-i)/2;
+            if(nums[idx]>=val){
+                j=idx-1;
+            } else {
+                i=idx+1;
+            } 
+        }
+        return i;
     }
 }
