09debc17207fe9d8d9e1b430711c788087b89f34
Mayank Daiya <74089887+Mayankdaiya@users.noreply.github.com>
2026-01-04 13:15:19 +0530

    Time: 9 ms (97.47%) | Memory: 46.3 MB (68.18%) - LeetSync

---
+++ 1284-four-divisors/four-divisors.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int sumFourDivisors(int[] nums) {
+        int res = 0;
+        for(int num : nums){
+            int t = sumOne(num);
+            if(t!=-1) res+=t;
+        }
+        return res;
+    }
+
+    private int sumOne(int n) {
+        int p = (int) Math.round(Math.cbrt(n));
+        if ((long) p * p * p == n && isPrime(p)) {
+            return 1 + p + p * p + p * p * p;
+        }
+        
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                int a = i;
+                int b = n / i;
+                if (a != b && isPrime(a) && isPrime(b)) {
+                    return 1 + a + b + n;
+                }
+                return -1;
+            }
+        }
+        return -1;
+    }
+
+    private boolean isPrime(int x) {
+        if (x < 2) return false;
+        for (int i = 2; i * i <= x; i++) {
+            if (x % i == 0) return false;
+        }
+        return true;
+    }
+}
